915aa4b35ac73731aba596b5163dd62b1eedc71b
= <=>
2020-05-12 07:32:06 +0800

    to windows

--- SpringInAction/springinaction/src/main/java/com/sxy/learning/spring/soundsystem/CDPlayerConfig.java
+++ SpringInAction/springinaction/src/main/java/com/sxy/learning/spring/soundsystem/CDPlayerConfig.java
@@ -1,12 +1,20 @@
 package com.sxy.learning.spring.soundsystem;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 扫描器
+ * 配置类
  */
 @Configuration
-@ComponentScan(basePackageClasses = {ScanFlag.class})
 public class CDPlayerConfig {
+
+    @Bean
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    public CDPlayer cdPlayer(){
+        return new
+    }
 }
